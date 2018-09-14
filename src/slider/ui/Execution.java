03ee92686bc087b48c9cdaa53f1e16642969a3ca
TERRIER <bastien.terrier7@gmail.com>
2018-09-14 12:06:30 +0200

    first line drawn

--- src/slider/ui/Execution.java
+++ src/slider/ui/Execution.java
@@ -2,6 +2,8 @@ package slider.ui;
 
 import javax.swing.plaf.basic.BasicSliderUI;
 
+import slider.fc.RangeSlider;
+
 import javax.swing.JFrame;
 import javax.swing.JSlider;
 
@@ -10,9 +12,13 @@ public class Execution {
 	public static void main(String[] args) {
 		JFrame J = new JFrame();
 		J.setVisible(true);
-		J.setSize(200, 200);
+		J.setSize(200, 250);
+		RangeSlider s = new RangeSlider(0, 9, 2, 3);
+		
+		RangeSliderUI js = new RangeSliderUI(s);
 //		JSlider js = new JSlider();
-//		J.add(js);
+		s.setUI(js);
+		J.add(s);
 	}
 	
 }
--- src/slider/ui/RangeSliderUI.java
+++ src/slider/ui/RangeSliderUI.java
@@ -1,6 +1,9 @@
 package slider.ui;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import javax.swing.JComponent;
 import javax.swing.JSlider;
 import javax.swing.plaf.basic.BasicSliderUI;
 import slider.fc.RangeSlider;
@@ -12,10 +15,29 @@ public class RangeSliderUI extends BasicSliderUI {
 	
 	public RangeSliderUI(JSlider b) {
 		super(b);
-		RangeSlider s = new RangeSlider(0, 9, 2, 3);
-		minRectangle = new Rectangle(2,2,10,10);
+		minRectangle = new Rectangle(2,9,100,100);
 		
 	}
 	
+	@Override
+	public void paint(Graphics g,JComponent c) {
+        Rectangle trackBounds = minRectangle;
+
+            int cy = (trackBounds.height / 2) - 2;
+            int cw = trackBounds.width;
+
+            g.translate(trackBounds.x, trackBounds.y + cy);
+
+            g.setColor(getShadowColor());
+            g.drawLine(0, 0, cw - 1, 0);
+            g.drawLine(0, 1, 0, 2);
+            g.setColor(getHighlightColor());
+            g.drawLine(0, 3, cw, 3);
+            g.drawLine(cw, 0, cw, 3);
+            g.setColor(Color.black);
+            g.drawLine(1, 1, cw-2, 1);
+
+            g.translate(-trackBounds.x, -(trackBounds.y + cy));
+	}
 
 }
