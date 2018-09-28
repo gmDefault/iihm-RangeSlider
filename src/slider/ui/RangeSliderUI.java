d60a993223b8f85208733dff936c9be44899bda2
gmDefault <cedric.lafrasse@gmail.com>
2018-09-28 16:34:56 +0200

    Adding collisions. Still one bug (left collision)

--- src/slider/ui/RangeSliderUI.java
+++ src/slider/ui/RangeSliderUI.java
@@ -52,7 +52,7 @@ public class RangeSliderUI extends BasicSliderUI {
 	protected int xPositionForValue( int value )    {
         int min = rs.getMin();
         int max = rs.getMax();
-        float quotient = ((float) tickRect.width * (float) value/ (float) (max-min));
+        float quotient = ((float) tickRect.width * ((float) value - (float) min)/ (float) (max-min));
         return (int) quotient;
 	    }
 
@@ -72,32 +72,34 @@ public class RangeSliderUI extends BasicSliderUI {
 
 		triangle_width = this.tickRect.width / 10;		
 
-		x = new int[] { xPositionForValue(posXRect1), xPositionForValue(posXRect1) ,
-				(int) ( xPositionForValue(posXRect1) + 10) };
+		x = new int[] { xPositionForValue(posXRect1), xPositionForValue(posXRect1),
+				(int) ( xPositionForValue(posXRect1) + 10 ) };
 
 		y = new int[] { tickRect.y - 20, tickRect.y , tickRect.y - 10  };
-		minRectangle = new Rectangle(xPositionForValue(posXRect1),tickRect.y - 20, 10, 20);
-		g.fillRect(xPositionForValue(posXRect1),tickRect.y - 20, 10, 20);
-//		System.out.println(xPositionForValue(posXRect1) + 10 + " " + tickRect.y);
+		
+		minRectangle = new Rectangle(xPositionForValue(posXRect1) -10,tickRect.y - 20, 20, 20);
+		
+		g.fillRect(xPositionForValue(posXRect1) +10,tickRect.y - 20, -10, 10);
+
 		Shape shape = new Polygon(x, y, 3);
 		graphics.setColor(Color.GREEN);
 		graphics.fill(shape);
 		graphics.setColor(Color.GRAY);
 		graphics.draw(shape);
 
-		x = new int[] { xPositionForValue(posXRect2), xPositionForValue(posXRect2) ,
-				(int) ( xPositionForValue(posXRect2) - 10) };
+		x = new int[] { xPositionForValue(posXRect2) + 10, xPositionForValue(posXRect2) + 10 ,
+				(int) ( xPositionForValue(posXRect2) ) };
 
 		y = new int[] { tickRect.y - 20, tickRect.y , tickRect.y - 10  };
 
-		g.fillRect(xPositionForValue(posXRect2) - 10 ,tickRect.y - 20, 10, 20);
+		g.fillRect(xPositionForValue(posXRect2) ,tickRect.y - 20, 10, 20);
 		shape = new Polygon(x, y, 3);
 		graphics.setColor(Color.GREEN);
 		graphics.fill(shape);
 		graphics.setColor(Color.GRAY);
 		graphics.draw(shape);
 		
-		maxRectangle = new Rectangle(xPositionForValue(posXRect2) - 10 ,tickRect.y - 20, 10, 20);
+		maxRectangle = new Rectangle(xPositionForValue(posXRect2) ,tickRect.y - 20, 10, 20);
 
 		
 	}
@@ -113,38 +115,44 @@ public class RangeSliderUI extends BasicSliderUI {
     }
 	
 	private class MyTrackListener extends TrackListener{
-		private Point Point_depart;
-		
-		
+	
 		public void mouseDragged(MouseEvent e) {
 			
 			if(movingRect1) {
-				float Value = (float) e.getX()*( (float) rs.getMax()- (float) rs.getMin())/((float) tickRect.width);
-				rs.setExtent(rs.getValue() - (int) Value + rs.getExtent()); 
-				rs.setValue((int) Value);
-				rs.repaint();
+				float Value = ((float) e.getX())*( (float) rs.getMax()- (float) rs.getMin())/((float) tickRect.width);
+				int ValueCasted = (int) Value + rs.getMin();
+				if( ValueCasted <= rs.getMax() && ValueCasted >= 0 && ValueCasted <= (rs.getValue() + rs.getExtent() - 20 )) {
+					System.out.println(Value);
+					rs.setExtent(rs.getValue() - ValueCasted + rs.getExtent()); 
+					rs.setValue(ValueCasted );
+					rs.repaint();
+				}
 			}
 			else if (movingRect2 ) {
-				float Extent = (float) e.getX()*( (float) rs.getMax()- (float) rs.getMin())/((float) tickRect.width);
-				rs.setExtent((int) Extent - rs.getValue() );
-				rs.repaint();
+				float Extent = ((float) e.getX())*( (float) rs.getMax()- (float) rs.getMin())/((float) tickRect.width);
+				int ExtentCasted = (int) Extent + rs.getMin();
+				if( ExtentCasted <= rs.getMax() && ExtentCasted >= 0 && ExtentCasted - rs.getValue() >= 0 ){
+					rs.setExtent(ExtentCasted - rs.getValue() );
+					System.out.println(ExtentCasted + " " + rs.getValue() + " " + rs.getExtent());
+					rs.repaint();
+				}
 			}
 		}
 		
 		public void mousePressed(MouseEvent e) {
-//			Click = new Rectangle(e.getX()-2, e.getY()-2, 20, 20);
-//			ClickOk = true;
 			if(minRectangle.contains(e.getX(), e.getY())) {
 				movingRect1 = true;
-
-
 			}
 			else if (maxRectangle.contains(e.getX(), e.getY())) {
 				System.out.println(e.getX() + " " + e.getY());
-
 				movingRect2 = true;
 
 			}
+		}
+		
+		public void mouseClicked(MouseEvent e) {
+			float Value = ((float) e.getX() + (float) rs.getMin())*( (float) rs.getMax()- (float) rs.getMin())/((float) tickRect.width);
+			System.out.println(Value);
 		}
 		
 		public void mouseReleased(MouseEvent e) {
