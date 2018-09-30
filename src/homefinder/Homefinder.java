3c7f7c415f81edf7123649e4402396962233aabc
TERRIER <bastien.terrier7@gmail.com>
2018-09-30 15:56:24 +0200

    Adding two RangeSliders in the  parametrization panel -> Home Finder (without interaction and dynamic redraw)

--- src/homefinder/Homefinder.java
+++ src/homefinder/Homefinder.java
@@ -1,10 +1,20 @@
 package homefinder;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Insets;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.border.Border;
+
+import slider.ui.JRangeSlider;
 
 public class Homefinder {
 	
@@ -13,13 +23,55 @@ public class Homefinder {
 	    Map map = new Map(450, 450);
 	 
 	    JFrame.setDefaultLookAndFeelDecorated(true);
-	    JFrame frame = new JFrame("JFrame Color Example");
-	    frame.setSize(500,500);
+	    JFrame frame = new JFrame("Home Finder - LAFRASSE & TERRIER");
+	    frame.setSize(800,500);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	 
+	    JPanel rightPanel = new JPanel();
+	    rightPanel.setLayout(new BorderLayout());
+	    
+	    JRangeSlider rs_room = new JRangeSlider("Number of rooms",0,100,25,25);
+	    JRangeSlider rs_price = new JRangeSlider("Range of price",30000,150000,35000,30000);
+
+
+		JPanel jp = new JPanel();
+		JPanel jp1 = new JPanel();
+		JPanel jp2 = new JPanel();
+		JPanel jp3 = new JPanel();
+
+		JLabel Title = new JLabel("Home Finder");
+		Title.setFont(new Font("Serif", Font.BOLD,25));
+
+		jp1.setLayout(new BoxLayout(jp1, BoxLayout.LINE_AXIS));
+		
+		jp1.add(Title);
+		
+		jp2.setLayout(new BoxLayout(jp2, BoxLayout.LINE_AXIS));
+		
+		jp2.add(rs_room);
+		
+		
+		jp3.setLayout(new BoxLayout(jp3, BoxLayout.LINE_AXIS));
+		
+		jp3.add(rs_price);
+		
+		
+		jp.setLayout(new BoxLayout(jp, BoxLayout.PAGE_AXIS));
+		
+		jp.add(jp1);
+		jp.add(jp2);
+		jp.add(jp3);
+
+		rightPanel.add(jp);
+		
+		
+	    
+	    
+	    JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,map,rightPanel);
+	    sp.setDividerLocation(500);
 	    // add panel to main frame
-	    frame.add(map);
-	 
+	    frame.add(sp);
+	    
 	    frame.setVisible(true);
 	 
 	  }
